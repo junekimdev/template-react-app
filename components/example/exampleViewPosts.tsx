99c33d09bd62cf27a00bfdbd10e718200220e487
June Kim <godlyjune@gmail.com>
2023-05-15 00:57:37 +0900

    Clarify types

--- components/example/exampleViewPosts.tsx
+++ components/example/exampleViewPosts.tsx
@@ -2,7 +2,7 @@ import styles from './example.module.scss';
 import { ExamplePost } from './exampleType';
 import PostSingle from './exampleViewPostSingle';
 
-const View = ({ data }) => {
+const View = ({ data }: { data: ExamplePost[] }) => {
   const dataMapper = (post: ExamplePost) => <PostSingle key={post.id} post={post} />;
 
   return <div className={styles.view}>{data.map(dataMapper)}</div>;

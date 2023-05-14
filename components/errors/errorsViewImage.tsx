a3e6c3c973cb52704d6a83d375a5e3bbb70f2219
June Kim <godlyjune@gmail.com>
2023-05-15 00:25:00 +0900

    Refactor for better readability

--- components/errors/errorsViewImage.tsx
+++ components/errors/errorsViewImage.tsx
@@ -7,31 +7,25 @@ import { code } from './errorsStates';
 const View = () => {
   const errorCode = useRecoilValue(code);
 
-  let ErrorImg: JSX.Element;
-  if (errorCode === 404)
-    ErrorImg = () => (
-      <Image
-        src="/assets/images/404_broken_robot.png"
-        alt="404 error image"
-        width="1024"
-        height="1024"
-        priority
-      />
-    );
-  if (errorCode >= 500)
-    ErrorImg = () => (
-      <Image
-        src="/assets/images/500_faulty_dog.png"
-        alt="Internal error image"
-        width="1024"
-        height="1024"
-        priority
-      />
-    );
-
   return (
     <div className={styles.errorImage}>
-      <ErrorImg />
+      {errorCode === 404 ? (
+        <Image
+          src="/assets/images/404_broken_robot.png"
+          alt="404 error image"
+          width="1024"
+          height="1024"
+          priority
+        />
+      ) : (
+        <Image
+          src="/assets/images/500_faulty_dog.png"
+          alt="Internal error image"
+          width="1024"
+          height="1024"
+          priority
+        />
+      )}
       <Link href="http://www.freepik.com" target="_blank" rel="noopener noreferrer">
         <span className={styles.imageLicenseTxt}>Designed by stories / Freepik</span>
       </Link>
